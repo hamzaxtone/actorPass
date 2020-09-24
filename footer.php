6b119779f6e5c63cd68d11491c2166121321f1c0
Muhammad Hamza <Muhammad.Hamza@aimviz.com>
2020-09-24 13:05:02 +0500

    V1 Files

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+    <!-- footer section -->
+    <footer>
+    </footer>
+
+
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="assets/js/jquery-3.1.1.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<!-- Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Carousel-min -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- Wow-min-js -->
+<script src="assets/js/wow.min.js"></script>
+<!-- masonry-grid-js -->
+<script src="assets/js/masonry.pkgd.min.js"></script>
+<!-- alertify-js -->
+<script src="assets/js/alertify.min.js"></script>
+<!-- juqeyr custom validatoin plugin-js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+<!-- custom validatoin-js -->
+<script src="assets/js/custom_validatiion.js"></script>
+<!-- script-js -->
+<script src="assets/js/script.js"></script>
+<!-- Custom-js -->
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title></title>
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- FontAwesome -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Animate -->
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <!-- Owl Slider -->
+    <link href="assets/css/owl.carousel.css" rel="stylesheet">
+    <!-- Owl Slider Theme -->
+    <link href="assets/css/owl.theme.css" rel="stylesheet">
+    <!--Jquery Validation -->
+    <link href="assets/css/validationEngine.jquery.css" rel="stylesheet">
+    <!--Jquery custom Validation -->
+    <link href="assets/css/custom_validatiion.css" rel="stylesheet">
+    <!-- style.css -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- custom Css Lins -->
+    <link href="assets/css/custom.css" rel="stylesheet">
+    <!-- Responsive -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+   
+
+
+        <!-- page header -->
+        <header>
+
+   
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					 
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button> <a class="navbar-brand" href="#">  <img src="../assets/images/logo_actor.png"></a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="#">Link</a>
+						</li>
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<form class="navbar-form navbar-left" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control" />
+						</div> 
+						<button type="submit" class="btn btn-default">
+							Submit
+						</button>
+					</form>
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				
+			</nav>
+		</div>
+	</div>
+</div>
+
+
+
+
+   
+   
+       
+           
+            
+</header>
